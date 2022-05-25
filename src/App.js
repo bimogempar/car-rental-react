b76ecf0926b86f26474437e51d4cfad5a01ca851
bimogempar <bimogempar@gmail.com>
2022-05-25 19:10:14 +0700

    first setup ant design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import 'antd/dist/antd.css';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
