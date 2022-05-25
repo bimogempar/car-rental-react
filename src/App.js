f11345c7218499df548fbf8e539e91b622cdb7d4
bimogempar <bimogempar@gmail.com>
2022-05-25 20:01:52 +0700

    add route for / and /cars

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import 'antd/dist/antd.min.css';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Cars from './components/Cars';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type="primary">Button</Button>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.min.css';
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,7 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
