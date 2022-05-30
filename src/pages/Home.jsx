4c4bb4c407cb8b59f7f9124299288e5328eec908
bimogempar <bimogempar@gmail.com>
2022-05-30 16:58:07 +0700

    fix conflict frontend from oauth api branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Layout, { Content, Footer, Header } from 'antd/lib/layout/layout'
 import React from 'react'
 import Login from '../components/Login'
 import Logout from '../components/Logout'
@@ -6,13 +5,9 @@ import Logout from '../components/Logout'
 
 export default function Home() {
     return (
-        <Layout>
-            <Header>Header</Header>
-            <Content>
-                <Login />
-                <Logout />
-            </Content>
-            <Footer>Footer</Footer>
-        </Layout>
+        <div>
+            <Login />
+            <Logout />
+        </div>
     )
 }
