32d668e5a6074de708d45d7703ec0f1adbc34d76
RizkiB12 <rbagus306@gmail.com>
2022-05-31 16:06:10 +0700

    add components cardcars

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import MyLayout from "../components/MyLayout";
 import SearchCar from "../components/SearchCar";
 import Hero from "../components/Hero";
+import CardCars from "../components/CardCars";
 
 export default function Cars() {
   return (
       <MyLayout>
         <Hero />
         <SearchCar />
+        <CardCars />
       </MyLayout>
   );
 }
