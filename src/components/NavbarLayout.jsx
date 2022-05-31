8d8ed6ee41ff7c9ae069d06d26a43d3fc0453b8f
RizkiB12 <rbagus306@gmail.com>
2022-05-31 18:38:53 +0700

    remake navbar

--- src/components/NavbarLayout.jsx
+++ src/components/NavbarLayout.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap'
 
 export default function NavbarLayout() {
   return (
     <Navbar bg="light" expand="lg" sticky='top'>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Car-Rental</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home">Our Service</Nav.Link>
+            <Nav.Link href="#link">Why Us</Nav.Link>
+            <Nav.Link href="#link">Testimonial</Nav.Link>
+            <Nav.Link href="#link">FAQ</Nav.Link>
           </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
